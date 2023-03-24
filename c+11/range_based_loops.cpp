283a1ef6f5d620492dc94785f03a9fa458e72fb9
Ignacio Perez <ignacio.perez.mahiques@gmail.com>
2023-03-24 10:16:56 +0100

    Added example of range based loops in c++11

---
+++ c+11/range_based_loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string texts[] = {"one", "two", "three", "four"};
+
+  // In range-based loop the object is directly given (not the index)
+  // Auto can be used to handle with different types (char, strings) and long types (e.g. iterators)
+  for (auto text: texts) {
+    for (auto c: text) {
+      cout << c << ", " << flush;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
